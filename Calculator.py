5615bcac08d6e38bb7c0c1b3cb43c54eddc388a2
KennonJavellana <85476641+KennonJavellana@users.noreply.github.com>
2021-07-08 13:51:25 +0800

    Update Calculator.py

--- Calculator.py
+++ Calculator.py
@@ -24,17 +24,17 @@ Button(root,text="7",command = lambda :get_variables(7)).grid(row=4,column=0, st
 Button(root,text=" 8",command = lambda :get_variables(8)).grid(row=4,column=1, sticky=N+S+E+W)
 Button(root,text=" 9",command = lambda :get_variables(9)).grid(row=4,column=2, sticky=N+S+E+W)
  
-#adding other buttons to the calculator
-Button(root,text="AC",command=lambda :clear_all()).grid(row=5,column=0, sticky=N+S+E+W)
-Button(root,text=" 0",command = lambda :get_variables(0)).grid(row=5,column=1, sticky=N+S+E+W)
-Button(root,text=" .",command=lambda :get_variables(".")).grid(row=5, column=2, sticky=N+S+E+W)
+#Adding other essential buttons to the Calculator
+Button(root, text = "AC", command = lambda: clear_all()).grid(row=5, column=0, sticky=N+S+E+W)
+Button(root, text = "0", command = lambda: clear_all()).grid(row=5, column=0, sticky=N+S+E+W)
+Button(root,text= ".", command = lambda: get_variables(".")).grid(row=5, column=2, sticky=N+S+E+W)
  
  
-Button(root,text="+",command= lambda :get_operation("+")).grid(row=2,column=3, sticky=N+S+E+W)
-Button(root,text="-",command= lambda :get_operation("-")).grid(row=3,column=3, sticky=N+S+E+W)
-Button(root,text="*",command= lambda :get_operation("*")).grid(row=4,column=3, sticky=N+S+E+W)
-Button(root,text="/",command= lambda :get_operation("/")).grid(row=5,column=3, sticky=N+S+E+W)
-Button(root,text="=",command= lambda :calculate()).grid(columnspan=6, sticky=N+S+E+W)
+Button(root,text= "+", command = lambda:get_operation("+")).grid(row=2, column=3, sticky=N+S+E+W)
+Button(root,text= "-", command = lambda:get_operation("-")).grid(row=3, column=3, sticky=N+S+E+W)
+Button(root,text= "*", command = lambda:get_operation("*")).grid(row=4, column=3, sticky=N+S+E+W)
+Button(root,text= "/", command = lambda:get_operation("/")).grid(row=5, column=3, sticky=N+S+E+W)
+Button(root,text= "=", command = lambda:calculate()).grid(columnspan=6, sticky=N+S+E+W)
 
 # i keeps the track of current position on the input text field
 i = 0
